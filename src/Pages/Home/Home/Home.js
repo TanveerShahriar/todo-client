818e4fc4a2d28a7fc42f1a46cb175ead8bc0df89
TanveerShahriar <tanveershahriara@gmail.com>
2022-05-19 02:19:24 +0600

    fetching task done

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container, Form } from 'react-bootstrap';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
 
 const Home = () => {
+    const [user] = useAuthState(auth);
+    const [tasks, setTasks] = useState([]);
+    const [refresh, setRefresh] = useState(true);
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/tasks?email=${user.email}`)
+            .then(res => res.json())
+            .then(data => setTasks(data));
+    }, [refresh]);
+
     const handleSubmit = event => {
         event.preventDefault();
         const name = event.target.name.value;
         const description = event.target.description.value;
+        const email = user.email;
         const done = false;
-        const task = { name, description, done };
+        const task = { name, description, email, done };
 
         const url = "http://localhost:5000/tasks";
         fetch(url, {
@@ -17,8 +30,10 @@ const Home = () => {
             },
             body: JSON.stringify(task)
         })
-        .then(res => res.json())
-        .then(data => {});
+            .then(res => res.json())
+            .then(data => { 
+                setRefresh(!refresh)
+            });
         event.target.reset();
     }
     return (
@@ -33,6 +48,21 @@ const Home = () => {
                 </Form.Group>
                 <button className="btn btn-outline-danger">ADD TASK</button>
             </Form>
+            <div>
+                {
+                    tasks.map(task => <div
+                        key={task._id}
+                        className="bg-danger rounded text-white text-start p-4 my-4 shadow-lg"
+                    >
+                        <div>
+                            <p className='fs-4'>Name : {task.name}</p>
+                            <p>Description : {task.description}</p>
+                        </div>
+                        <button className="btn btn-outline-light me-3">Completed</button>
+                        <button className="btn btn-outline-light">Delete</button>
+                    </div>)
+                }
+            </div>
         </Container>
     );
 };
