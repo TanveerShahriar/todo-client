880ad8598264484d84e6c57d252ec7cb5a9e2395
TanveerShahriar <tanveershahriara@gmail.com>
2022-05-19 01:18:12 +0600

    task adding done

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,39 @@
 import React from 'react';
+import { Container, Form } from 'react-bootstrap';
 
 const Home = () => {
+    const handleSubmit = event => {
+        event.preventDefault();
+        const name = event.target.name.value;
+        const description = event.target.description.value;
+        const done = false;
+        const task = { name, description, done };
+
+        const url = "http://localhost:5000/tasks";
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(task)
+        })
+        .then(res => res.json())
+        .then(data => {});
+        event.target.reset();
+    }
     return (
-        <div>
-            this is home
-        </div>
+        <Container>
+            <h1 className='my-5 text-danger'>To Do List</h1>
+            <Form onSubmit={handleSubmit}>
+                <Form.Group className="mb-3" controlId="formBasicName">
+                    <Form.Control type="text" placeholder="Add Task Name" name="name" required />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                    <Form.Control as="textarea" placeholder='Add Task Description' name="description" rows={3} required />
+                </Form.Group>
+                <button className="btn btn-outline-danger">ADD TASK</button>
+            </Form>
+        </Container>
     );
 };
 
